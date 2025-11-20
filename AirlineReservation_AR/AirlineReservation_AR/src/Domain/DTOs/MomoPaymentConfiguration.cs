2791c1ba7683280e3e0212a708cdabd8b281c5c0
kats205 <npbaokhanhpro@gmail.com>
2025-11-21 02:04:09 +0700

    feat Momo Payment

---
+++ AirlineReservation_AR/AirlineReservation_AR/src/Domain/DTOs/MomoPaymentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MomoQR
+{
+    public class MomoPaymentConfiguration
+    {
+        public string partnerCode { get; set; }
+        public string partnerName { get; set; }
+        public string storeId { get; set; }
+        public string requestId { get; set; }
+        public long amount { get; set; }
+        public string orderId { get; set; }
+        public string orderInfo { get; set; }
+        public string redirectUrl { get; set; }
+        public string ipnUrl { get; set; }
+        public string lang { get; set; } = "vi";
+        public string extraData { get; set; } = "";
+        public string requestType { get; set; } = "captureWallet";
+        public string signature { get; set; }
+    }
+}
---
+++ AirlineReservation_AR/AirlineReservation_AR/src/Presentation (Winform)/Views/Forms/MomoQR.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MomoQR
+{
+    public partial class MomoQR : Form
+    {
+        public MomoQR()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtAmount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private async void payButton_Click(object sender, EventArgs e)
+        {
+            string amount = txtAmount.Text.Trim();
+            if (string.IsNullOrEmpty(amount))
+            {
+                MessageBox.Show("Please enter an amount!", "Validation Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (long.TryParse(amount, out long amountValue) && amountValue > 0)
+            {
+                MessageBox.Show($"Processing payment of {amountValue:N0} VND...\n\nRedirecting to Momo...",
+                    "Payment Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                var momo = new MomoService();
+                await momo.CreatePaymentAsync(amountValue);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid amount!", "Validation Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void payButton_MouseEnter(object sender, EventArgs e)
+        {
+            payButton.BackColor = Color.FromArgb(200, 35, 51);
+        }
+
+        private void payButton_MouseLeave(object sender, EventArgs e)
+        {
+            payButton.BackColor = Color.FromArgb(220, 53, 69);
+        }
+    }
+}
